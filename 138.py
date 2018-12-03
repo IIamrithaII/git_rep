c76f61bdb84bbe32bd55d489d1612c288053fda8
IIamrithaII <amrithanandini2003@gmail.com>
2018-12-03 18:50:33 +0530

    Initial commit

---
+++ 138.py
@@ -0,0 +1,11 @@
+import turtle
+a=0
+while a <4 :
+    if (a == 1) or (a==3):
+        turtle.color('red')
+    else:
+    turtle.color('black')    
+    turtle.fd(100)
+    turtle.rt(90)
+    a= a+1
+    
